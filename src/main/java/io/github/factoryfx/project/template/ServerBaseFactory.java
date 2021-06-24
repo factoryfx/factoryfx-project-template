c21c92058f733e7f643ad0442c5d69749209bcca
hbrackmann <henning.brackmann@scoop-software.de>
2021-06-24 18:27:39 +0200

    gradle upgrade

--- src/main/java/io/github/factoryfx/project/template/ServerBaseFactory.java
+++ src/main/java/io/github/factoryfx/project/template/ServerBaseFactory.java
@@ -4,6 +4,8 @@ import io.github.factoryfx.factory.SimpleFactoryBase;
 import io.github.factoryfx.jetty.builder.JettyServerRootFactory;
 
 /**
- * Base factory for all factories in the project */
+ * Base factory for all factories in the project
+ * Usage  requires one less generic parameter to be specified
+ * */
 public abstract class ServerBaseFactory<L> extends SimpleFactoryBase<L, JettyServerRootFactory> {
 }
